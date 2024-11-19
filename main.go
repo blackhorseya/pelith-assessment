10636f4efd371a755d2f29cdcb00164994ba91dc
Sean Zheng <blackhorseya@gmail.com>
2024-11-19 16:05:53 +0800

    feat: implement command management for application behavior
    - Add a new command file for managing the application behavior
    - Update go.mod to include dependencies for cobra and viper
    - Modify main.go to execute the command from the new command file instead of printing a message
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-)
+import "github.com/blackhorseya/pelith-assessment/cmd"
 
 func main() {
-	fmt.Println("Hello, World!")
+	cmd.Execute()
 }
