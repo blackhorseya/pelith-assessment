895492795e4106a33f9c8dcb6b07e8e304448029
Sean Zheng <blackhorseya@gmail.com>
2024-11-20 06:39:57 +0800

    feat: enhance error handling and context management
    - Add context handling using the `contextx` package for the Start and Shutdown methods
    - Remove TODO comments for future implementation in the Start and Shutdown methods
    - Handle errors by logging them with the `zap` logger in the Start and Shutdown methods
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/server/impl.go
+++ cmd/server/impl.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/blackhorseya/pelith-assessment/internal/shared/httpx"
 	"github.com/blackhorseya/pelith-assessment/pkg/adapterx"
+	"github.com/blackhorseya/pelith-assessment/pkg/contextx"
+	"go.uber.org/zap"
 )
 
 type impl struct {
@@ -20,11 +22,25 @@ func newImpl(injector *injector, ginServer *httpx.GinServer) adapterx.Server {
 }
 
 func (i *impl) Start(c context.Context) error {
-	// TODO: 2024/11/20|sean|implement me
+	ctx := contextx.WithContext(c)
+
+	err := i.ginServer.Start(ctx)
+	if err != nil {
+		ctx.Error("gin server start failed", zap.Error(err))
+		return err
+	}
+
 	return nil
 }
 
 func (i *impl) Shutdown(c context.Context) error {
-	// TODO: 2024/11/20|sean|implement me
+	ctx := contextx.WithContext(c)
+
+	err := i.ginServer.Stop(ctx)
+	if err != nil {
+		ctx.Error("gin server stop failed", zap.Error(err))
+		return err
+	}
+
 	return nil
 }
