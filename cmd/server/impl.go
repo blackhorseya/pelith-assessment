407d1f9debaa0a051b50b97f2c9766dc58e63052
Sean Zheng <blackhorseya@gmail.com>
2024-11-20 06:24:20 +0800

    feat: implement new server architecture with dependency injection
    - Implement a new server structure with a `newImpl` function
    - Add a `Start` and `Shutdown` method with TODO comments for future implementation
    - Introduce a new `wire.go` file for dependency injection setup
    - Create a generated `wire_gen.go` file for wire injectors
    - Update the `start.go` file to include a command for starting the server
    - Add `github.com/google/wire` as a dependency in `go.mod`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/server/impl.go
+++ cmd/server/impl.go
@@ -1,4 +1,24 @@
 package server
 
+import (
+	"context"
+
+	"github.com/blackhorseya/pelith-assessment/pkg/adapterx"
+)
+
 type impl struct {
+}
+
+func newImpl() adapterx.Server {
+	return &impl{}
+}
+
+func (i *impl) Start(c context.Context) error {
+	// TODO: 2024/11/20|sean|implement me
+	return nil
+}
+
+func (i *impl) Shutdown(c context.Context) error {
+	// TODO: 2024/11/20|sean|implement me
+	return nil
 }
