31dcdef55101c2f7611925fb027dd2c27f8c356a
Sean Zheng <blackhorseya@gmail.com>
2024-11-20 05:56:39 +0800

    feat: update Go module dependencies and server initialization
    - Update the Go module dependencies to newer versions
    - Remove the old version of the `golang.org/x/sys` and `golang.org/x/text` packages
    - Add new indirect dependencies for `golang.org/x/net` and `google.golang.org/protobuf`
    - Introduce new functions and types in the `server.go` file for initializing routes and defining the Gin server structure
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/shared/httpx/server.go
+++ internal/shared/httpx/server.go
@@ -1,1 +1,18 @@
 package httpx
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// InitRoutes is a function to initialize routes.
+type InitRoutes func(router *gin.Engine)
+
+// GinServer is an HTTP server.
+type GinServer struct {
+	httpserver *http.Server
+
+	// Router is the gin engine.
+	Router *gin.Engine
+}
