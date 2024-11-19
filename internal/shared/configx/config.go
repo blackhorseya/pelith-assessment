edbc272aaf8f153ed7869242cc78663fc7191c14
Sean Zheng <blackhorseya@gmail.com>
2024-11-19 16:29:57 +0800

    feat: enhance configuration management with YAML support
    - Introduce a new function to load configuration from a YAML file
    - Add error handling for reading and unmarshalling the configuration
    - Initialize the logger based on the configuration values
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/shared/configx/config.go
+++ internal/shared/configx/config.go
@@ -12,3 +12,29 @@ type Configx struct {
 	// Logger is the logger configuration.
 	Logger logger.Options `json:"logger" yaml:"logger" mapstructure:"logger"`
 }
+
+// LoadConfig is used to load the configuration.
+func LoadConfig(path string) (*Configx, error) {
+	v := viper.GetViper()
+	v.SetConfigFile(path)
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+
+	// Load the configuration file
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config *Configx
+	if err := v.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+	config.viper = v
+
+	// Set the logger
+	if err := logger.Init(config.Logger); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
