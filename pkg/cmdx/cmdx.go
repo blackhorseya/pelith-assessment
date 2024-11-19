214fed51e329f5fc0e4a584f4d1b7c9fc3e8bfcc
Sean Zheng <blackhorseya@gmail.com>
2024-11-20 06:14:33 +0800

    feat: implement server and command interface packages
    - Add a new file for the adapterx package that defines a Server interface with Start and Shutdown methods.
    - Create a new file for the cmdx package that implements a ServiceCmd struct and methods for creating and handling service commands.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/cmdx/cmdx.go
@@ -0,0 +1,51 @@
+package cmdx
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/blackhorseya/pelith-assessment/pkg/adapterx"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// ServiceCmd represents the service command.
+type ServiceCmd struct {
+	Use        string
+	Short      string
+	GetService func(v *viper.Viper) (adapterx.Server, func(), error)
+}
+
+// NewServiceCmd creates a new service command.
+func NewServiceCmd(use string, short string, svc func(v *viper.Viper) (adapterx.Server, func(), error)) *cobra.Command {
+	return (&ServiceCmd{Use: use, Short: short, GetService: svc}).NewCmd()
+}
+
+// NewCmd creates a new service command.
+func (c *ServiceCmd) NewCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   c.Use,
+		Short: c.Short,
+		Run: func(cmd *cobra.Command, args []string) {
+			v := viper.GetViper()
+
+			service, clean, err := c.GetService(v)
+			cobra.CheckErr(err)
+			defer clean()
+
+			ctx := context.Background()
+			err = service.Start(ctx)
+			cobra.CheckErr(err)
+
+			signalChan := make(chan os.Signal, 1)
+			signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+
+			<-signalChan
+
+			err = service.Shutdown(ctx)
+			cobra.CheckErr(err)
+		},
+	}
+}
