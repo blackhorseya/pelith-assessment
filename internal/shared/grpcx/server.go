9e3e897e1c22e31722c0482456e4f2f1c044c994
Sean Zheng <blackhorseya@gmail.com>
2024-11-20 10:30:41 +0800

    feat: implement gRPC server initialization and dependencies
    - Add a new dependency for gRPC version 1.64.1 in go.mod
    - Add a new indirect dependency for googleapis RPC in go.mod
    - Import the gRPC package in server.go
    - Define a function type for initializing gRPC servers in server.go
    - Create a Server struct to represent the gRPC server in server.go
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/shared/grpcx/server.go
+++ internal/shared/grpcx/server.go
@@ -1,1 +1,14 @@
 package grpcx
+
+import (
+	"google.golang.org/grpc"
+)
+
+// InitServers define register handler
+type InitServers func(s *grpc.Server)
+
+// Server represents the grpc server.
+type Server struct {
+	grpcserver *grpc.Server
+	addr       string
+}
