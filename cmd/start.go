f94f16016e9c8622f2dc04b5847a0750f486362b
Sean Zheng <blackhorseya@gmail.com>
2024-11-22 11:33:31 +0800

    feat: add token support for Etherscan API in startCmd
    - Add a `token` flag for the Etherscan API in the `startCmd` command
    - Bind the `token` flag to the configuration using `viper`
    - Maintain the addition of `startCmd` to `rootCmd` after the new flag setup
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/start.go
+++ cmd/start.go
@@ -4,6 +4,7 @@ import (
 	"github.com/blackhorseya/pelith-assessment/cmd/server"
 	"github.com/blackhorseya/pelith-assessment/pkg/cmdx"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var startCmd = &cobra.Command{
@@ -12,7 +13,10 @@ var startCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(startCmd)
+	startCmd.PersistentFlags().String("token", "", "The token for Etherscan API")
+	_ = viper.BindPFlag("services.server.etherscan.api_key", startCmd.PersistentFlags().Lookup("token"))
 
 	startCmd.AddCommand(cmdx.NewServiceCmd("server", "Start the server", server.NewCmd))
+
+	rootCmd.AddCommand(startCmd)
 }
