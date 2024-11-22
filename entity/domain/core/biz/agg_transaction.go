87525312976b81e60d16b1146ff437e6ed5286ed
Sean Zheng <blackhorseya@gmail.com>
2024-11-22 10:01:19 +0800

    feat: implement swap transaction support and queries
    - Add a new function `NewSwapTransaction` to create swap transactions.
    - Introduce a new struct `SwapDetails` for providing details about swap transactions.
    - Change the data types of transaction fields from byte slices to strings for `TxHash`, `FromAddress`, and `ToAddress`.
    - Add an enumeration `TransactionType` with different types of transactions.
    - Update the `Transaction` message to include the new `SwapDetails` for swap transactions.
    - Introduce a new file for transaction queries.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/core/biz/agg_transaction.go
+++ entity/domain/core/biz/agg_transaction.go
@@ -1,12 +1,24 @@
 package biz
 
 import (
+	"errors"
+
 	"github.com/blackhorseya/pelith-assessment/entity/domain/core/model"
 )
 
 // Transaction is an aggregate root that represents the transaction.
 type Transaction struct {
 	model.Transaction
+}
+
+// NewSwapTransaction is used to create a new swap transaction.
+func NewSwapTransaction(
+	txHash, fromAddress, toAddress []byte,
+	amount float64,
+	swapDetails *model.SwapDetails,
+) (*Transaction, error) {
+	// TODO: 2024/11/22|sean|implement the NewTransaction
+	return nil, errors.New("not implemented")
 }
 
 // TransactionList is a list of transactions.
