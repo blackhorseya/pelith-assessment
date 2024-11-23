1f1c19109307781d3ca6ebe0f97ae371c2bffd7b
Sean Zheng <blackhorseya@gmail.com>
2024-11-23 09:36:57 +0800

    feat: enhance transaction fetching and context management
    - Import the `time` package for handling time-related functions.
    - Add the `contextx` package for better context management.
    - Implement the logic to fetch the start and end blocks based on transaction conditions.
    - Restore the code to handle transaction fetching by address.
    - Return the results of the transactions instead of an error indicating the function is not implemented.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/domain/core/infra/etherscan/transaction_repo_impl.go
+++ internal/domain/core/infra/etherscan/transaction_repo_impl.go
@@ -2,12 +2,16 @@ package etherscan
 
 import (
 	"context"
-	"errors"
+	"time"
 
 	"github.com/blackhorseya/pelith-assessment/entity/domain/core/biz"
+	"github.com/blackhorseya/pelith-assessment/entity/domain/core/model"
 	"github.com/blackhorseya/pelith-assessment/internal/domain/core/app/query"
 	"github.com/blackhorseya/pelith-assessment/internal/shared/configx"
+	"github.com/blackhorseya/pelith-assessment/pkg/contextx"
 	"github.com/nanmu42/etherscan-api"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // TransactionRepoImpl is the implementation of TransactionRepo.
@@ -34,54 +38,53 @@ func (i *TransactionRepoImpl) ListByAddress(
 	address string,
 	cond query.ListTransactionCondition,
 ) (item biz.TransactionList, total int, err error) {
-	// ctx := contextx.WithContext(c)
-	//
-	// startBlock, err := i.client.BlockNumber(cond.StartTime.Unix(), "after")
-	// if err != nil {
-	// 	ctx.Error("failed to fetch start block", zap.Error(err), zap.Time("start_time", cond.StartTime))
-	// 	return nil, 0, err
-	// }
-	//
-	// // if cond.EndTime > now then set endBlock = now
-	// if cond.EndTime.After(time.Now()) {
-	// 	cond.EndTime = time.Now()
-	// }
-	// endBlock, err := i.client.BlockNumber(cond.EndTime.Unix(), "before")
-	// if err != nil {
-	// 	ctx.Error("failed to fetch end block", zap.Error(err), zap.Time("end_time", cond.EndTime))
-	// 	return nil, 0, err
-	// }
-	//
-	// txs, err := i.client.NormalTxByAddress(address, &startBlock, &endBlock, 1, 100, true)
-	// if err != nil {
-	// 	ctx.Error("failed to fetch transactions", zap.Error(err))
-	// 	return nil, 0, err
-	// }
-	//
-	// var res biz.TransactionList
-	// for _, tx := range txs {
-	// 	res = append(res, &biz.Transaction{
-	// 		Transaction: model.Transaction{
-	// 			TxHash:      tx.Hash,
-	// 			FromAddress: tx.From,
-	// 			ToAddress:   tx.To,
-	// 			Amount:      tx.Value.Int().Int64(),
-	// 			Timestamp:   timestamppb.New(tx.TimeStamp.Time()),
-	// 			TaskId:      nil,
-	// 			CampaignId:  nil,
-	// 			Status:      model.TransactionStatus_TRANSACTION_STATUS_COMPLETED,
-	// 			Type:        0,
-	// 			SwapDetails: &model.SwapDetails{
-	// 				FromTokenAddress: "",
-	// 				ToTokenAddress:   "",
-	// 				FromTokenAmount:  0,
-	// 				ToTokenAmount:    0,
-	// 				PoolAddress:      "",
-	// 			},
-	// 		},
-	// 	})
-	// }
-	//
-	// return res, len(res), nil
-	return nil, 0, errors.New("not implemented")
+	ctx := contextx.WithContext(c)
+
+	startBlock, err := i.client.BlockNumber(cond.StartTime.Unix(), "after")
+	if err != nil {
+		ctx.Error("failed to fetch start block", zap.Error(err), zap.Time("start_time", cond.StartTime))
+		return nil, 0, err
+	}
+
+	// if cond.EndTime > now then set endBlock = now
+	if cond.EndTime.After(time.Now()) {
+		cond.EndTime = time.Now()
+	}
+	endBlock, err := i.client.BlockNumber(cond.EndTime.Unix(), "before")
+	if err != nil {
+		ctx.Error("failed to fetch end block", zap.Error(err), zap.Time("end_time", cond.EndTime))
+		return nil, 0, err
+	}
+
+	txs, err := i.client.NormalTxByAddress(address, &startBlock, &endBlock, 1, 100, true)
+	if err != nil {
+		ctx.Error("failed to fetch transactions", zap.Error(err))
+		return nil, 0, err
+	}
+
+	var res biz.TransactionList
+	for _, tx := range txs {
+		res = append(res, &biz.Transaction{
+			Transaction: model.Transaction{
+				TxHash:      tx.Hash,
+				FromAddress: tx.From,
+				ToAddress:   tx.To,
+				Amount:      tx.Value.Int().Int64(),
+				Timestamp:   timestamppb.New(tx.TimeStamp.Time()),
+				TaskId:      nil,
+				CampaignId:  nil,
+				Status:      model.TransactionStatus_TRANSACTION_STATUS_COMPLETED,
+				Type:        0,
+				SwapDetails: &model.SwapDetails{
+					FromTokenAddress: "",
+					ToTokenAddress:   "",
+					FromTokenAmount:  0,
+					ToTokenAmount:    0,
+					PoolAddress:      "",
+				},
+			},
+		})
+	}
+
+	return res, len(res), nil
 }
