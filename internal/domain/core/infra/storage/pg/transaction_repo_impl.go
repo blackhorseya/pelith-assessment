225bd42f831d8843b53e6ab75d707f4744af3247
Sean Zheng <blackhorseya@gmail.com>
2024-11-24 20:06:46 +0800

    feat: implement transaction repository functionality
    - Add import for context package
    - Add import for biz package
    - Introduce NewTransactionRepoImpl function to create a new TransactionRepoImpl instance
    - Implement Create method with a placeholder for future implementation
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/domain/core/infra/storage/pg/transaction_repo_impl.go
+++ internal/domain/core/infra/storage/pg/transaction_repo_impl.go
@@ -1,5 +1,21 @@
 package pg
 
+import (
+	"context"
+
+	"github.com/blackhorseya/pelith-assessment/entity/domain/core/biz"
+)
+
 // TransactionRepoImpl represents the PostgreSQL implementation of the TransactionRepo.
 type TransactionRepoImpl struct {
+}
+
+// NewTransactionRepoImpl creates a new TransactionRepoImpl.
+func NewTransactionRepoImpl() *TransactionRepoImpl {
+	return &TransactionRepoImpl{}
+}
+
+func (i *TransactionRepoImpl) Create(c context.Context, transaction *biz.Transaction) error {
+	// TODO: 2024/11/24|sean|implement me
+	panic("implement me")
 }
