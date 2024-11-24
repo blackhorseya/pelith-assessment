285d683a97d73999282662b425dc4e1cc75be36e
Sean Zheng <blackhorseya@gmail.com>
2024-11-24 12:43:18 +0800

    refactor: refactor test result handling and clean up code
    - Remove unused import of strconv
    - Commented out a loop for sending test results to channel
    - Retain the TODO comment for implementing the RunBacktest method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/domain/core/biz/backtest_service_impl.go
+++ internal/domain/core/biz/backtest_service_impl.go
@@ -2,7 +2,6 @@ package biz
 
 import (
 	"context"
-	"strconv"
 
 	"github.com/blackhorseya/pelith-assessment/entity/domain/core/biz"
 	"github.com/blackhorseya/pelith-assessment/entity/domain/core/model"
@@ -22,16 +21,5 @@ func (i *backtestServiceImpl) RunBacktest(
 	resultCh chan<- *model.BacktestResult,
 ) error {
 	// TODO: 2024/11/24|sean|Implement the RunBacktest method
-	// send test result to channel
-	for idx := 0; idx < 10; idx++ {
-		resultCh <- &model.BacktestResult{
-			UserId:       "test" + strconv.Itoa(idx),
-			TotalSwaps:   0,
-			TotalVolume:  0,
-			PointsEarned: 0,
-			TaskProgress: nil,
-		}
-	}
-
 	return nil
 }
