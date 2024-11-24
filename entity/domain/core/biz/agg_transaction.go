1dd8db87c5a1bd34dd21cd2c40dbe1da0f74aeea
Sean Zheng <blackhorseya@gmail.com>
2024-11-24 11:31:53 +0800

    feat: implement transaction processing and error handling
    - Add context and error imports to the file
    - Implement the IsSwapExecuted method to check for swap transactions
    - Add the Process method to handle transaction processing and return an event or error
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/core/biz/agg_transaction.go
+++ entity/domain/core/biz/agg_transaction.go
@@ -1,12 +1,32 @@
 package biz
 
 import (
+	"context"
+	"errors"
+
 	"github.com/blackhorseya/pelith-assessment/entity/domain/core/model"
+	"github.com/blackhorseya/pelith-assessment/pkg/eventx"
 )
 
 // Transaction is an aggregate root that represents the transaction.
 type Transaction struct {
 	model.Transaction
+}
+
+// IsSwapExecuted is used to check if the transaction is swap executed.
+func (tx *Transaction) IsSwapExecuted() bool {
+	return tx.Type == model.TransactionType_TRANSACTION_TYPE_SWAP
+}
+
+// Process is used to process the transaction.
+func (tx *Transaction) Process(c context.Context) (eventx.DomainEvent, error) {
+	if tx.IsSwapExecuted() {
+		return NewSwapExecutedEvent(tx.Timestamp.AsTime(), SwapExecutedPayload{
+			TxID: tx.TxHash,
+		}), nil
+	}
+
+	return nil, errors.New("unsupported transaction type")
 }
 
 // TransactionList is a list of transactions.
