8a6e0bd3997711710f87ad37915910a5631be151
Sean Zheng <blackhorseya@gmail.com>
2024-11-25 11:31:35 +0800

    feat: add data access objects for transactions and swaps
    - Introduce a new file for transaction repository data access objects
    - Define the TransactionDAO struct for handling transaction-related data
    - Define the SwapEventDAO struct for handling swap event-related data
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/domain/core/infra/storage/pg/transaction_repo_dao.go
@@ -0,0 +1,29 @@
+package pg
+
+import (
+	"time"
+)
+
+// TransactionDAO represents the transactions table
+type TransactionDAO struct {
+	TxHash      string    `db:"tx_hash"`      // TransactionDAO hash
+	BlockNumber int       `db:"block_number"` // Block number
+	Timestamp   time.Time `db:"timestamp"`    // TransactionDAO timestamp
+	FromAddress string    `db:"from_address"` // Sender address
+	ToAddress   string    `db:"to_address"`   // Receiver address
+	Value       float64   `db:"value"`        // TransactionDAO value
+	GasUsed     int       `db:"gas_used"`     // Gas used
+	GasPrice    float64   `db:"gas_price"`    // Gas price
+	Status      bool      `db:"status"`       // TransactionDAO status (success or failure)
+}
+
+// SwapEventDAO represents the swap_events table
+type SwapEventDAO struct {
+	ID               int    `db:"id"`                 // Primary key
+	TxHash           string `db:"tx_hash"`            // Associated transaction hash
+	FromTokenAddress string `db:"from_token_address"` // Source token address
+	ToTokenAddress   string `db:"to_token_address"`   // Destination token address
+	FromTokenAmount  string `db:"from_token_amount"`  // Source token amount
+	ToTokenAmount    string `db:"to_token_amount"`    // Destination token amount
+	PoolAddress      string `db:"pool_address"`       // Swap pool address (if applicable)
+}
