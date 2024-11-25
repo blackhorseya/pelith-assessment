588e127ebeca335ca0c0d06a5467e926c077102e
Sean Zheng <blackhorseya@gmail.com>
2024-11-25 12:58:47 +0800

    feat: improve error handling and SQL file organization
    - Enhance error handling by providing more descriptive error messages during PostgreSQL driver initialization and migration processes.
    - Introduce a new file to embed migration SQL files for better organization and accessibility.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/domain/core/infra/storage/pg/utils.go
+++ internal/domain/core/infra/storage/pg/utils.go
@@ -2,29 +2,41 @@ package pg
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/blackhorseya/pelith-assessment/scripts/migrations"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/jmoiron/sqlx"
 )
 
 func migrateUp(db *sqlx.DB, name string) error {
+	// Initialize the PostgreSQL driver
 	driver, err := postgres.WithInstance(db.DB, &postgres.Config{
 		MigrationsTable:       name + "_migrations",
 		MultiStatementEnabled: true,
 	})
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to initialize postgres driver: %w", err)
 	}
 
-	migration, err := migrate.NewWithDatabaseInstance(migrationFolder+"/"+name, "postgres", driver)
+	// Initialize the iofs driver with the embedded migrations
+	sourceDriver, err := iofs.New(migrations.MigrationsFS, name)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to initialize iofs driver: %w", err)
 	}
 
+	// Create a new migrate instance
+	migration, err := migrate.NewWithInstance("iofs", sourceDriver, "postgres", driver)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	// Run the migrations
 	err = migration.Up()
 	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return err
+		return fmt.Errorf("migration failed: %w", err)
 	}
 
 	return nil
