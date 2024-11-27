b2b5ca745e37f6db361cf687026e8acb8868f4a5
Sean Zheng <blackhorseya@gmail.com>
2024-11-27 11:09:50 +0800

    feat: add HTML templating system for dynamic content
    - Add a new HTML template for the base layout
    - Create a tasks template that extends the base template
    - Implement a Go file to manage the HTML templates and serve static files
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ web/web.go
@@ -0,0 +1,21 @@
+package web
+
+import (
+	"embed"
+	"html/template"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed templates/*
+var f embed.FS
+
+var templates = template.Must(template.ParseFS(f, "templates/*"))
+
+// SetHTMLTemplate set html template.
+func SetHTMLTemplate(r *gin.Engine) {
+	r.SetHTMLTemplate(templates)
+
+	r.StaticFS("/static", http.FS(f))
+}
