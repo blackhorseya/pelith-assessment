459dae5169e4562e4863cddc837c123f5d673132
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 10:27:31 +0800

    feat: implement campaign creation functionality
    - Add import statements for context and necessary packages
    - Introduce the `NewCampaignCreator` function for creating campaign creators
    - Implement the `Create` method for the `CampaignRepoImpl` struct with a placeholder for future implementation
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/domain/infra/storage/mongodb/campaign_repo_impl.go
+++ internal/domain/infra/storage/mongodb/campaign_repo_impl.go
@@ -1,5 +1,12 @@
 package mongodb
 
+import (
+	"context"
+
+	"github.com/blackhorseya/pelith-assessment/entity/domain/core/biz"
+	"github.com/blackhorseya/pelith-assessment/internal/domain/repo"
+)
+
 // CampaignRepoImpl is a struct to define the campaign repository implementation
 type CampaignRepoImpl struct {
 }
@@ -7,4 +14,14 @@ type CampaignRepoImpl struct {
 // NewCampaignRepoImpl is a function to create a new campaign repository implementation
 func NewCampaignRepoImpl() *CampaignRepoImpl {
 	return &CampaignRepoImpl{}
+}
+
+// NewCampaignCreator is a function to create a new campaign creator
+func NewCampaignCreator(impl *CampaignRepoImpl) repo.CampaignCreator {
+	return impl
+}
+
+func (i *CampaignRepoImpl) Create(c context.Context, campaign *biz.Campaign) error {
+	// TODO: 2024/12/1|sean|implement me
+	panic("implement me")
 }
